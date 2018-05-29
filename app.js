2e9df06f300b77e94ce55e2ef05d47bd9836fb14
Chuck Lu <chuck.lu@qq.com>
2018-05-29 13:43:33 +0800

    create a Node.js project by vs2017

---
+++ app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var car = null;
+var type = typeof car;
+console.log(type);
+
+var message;
+console.log(message);
+var type2 = typeof message;
+console.log(type2);
+
+console.log('Hello world');
+
