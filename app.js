750df34e79236160f028f6b538bbdf8acc09ccf9
Chuck Lu <chuck.lu@qq.com>
2018-06-22 16:09:20 +0800

    check the parameter is digit or not

--- app.js
+++ app.js
@@ -11,3 +11,8 @@ console.log(type2);
 
 console.log('Hello world');
 
+//https://www.codewars.com/kata/is-it-a-number/train/javascript
+function isDigit(s) {
+    return !isNaN(parseFloat(s)) && !isNaN(s - 0);
+}
+
